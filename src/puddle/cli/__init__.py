37790075d7c85a62e628c25cfff70ea7c9d3a9cc
Zebulun Arendsee <arendsee@users.noreply.github.com>
2024-06-06 08:45:44 -0400

    Add LLM question command
    The command
    
    ```
    puddle ask "this is a prompt"
    ```
    
    Will return the LLM content.
    
    Currently the model is set at gpt-4, the parser is set as the string
    identity parser, and the role is set to be a museum curator.
    
    I am reconsidering whether I actually want to use langchain. I think
    they have a bit too much layering.
    
    So next up, I'll remove the langchain deps and then start implementing
    the vector database integration.

--- src/puddle/cli/__init__.py
+++ src/puddle/cli/__init__.py
@@ -1,12 +1,13 @@
-# SPDX-FileCopyrightText: 2024-present Zebulun Arendsee <arendsee@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
 import click
-
 from puddle.__about__ import __version__
+from puddle.model import ask
 
-
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=False)
 @click.version_option(version=__version__, prog_name="puddle")
 def puddle():
-    click.echo("Hello world!")
+    pass
+
+@puddle.command(name = "ask")
+@click.argument("prompt", type=str)
+def ask_cmd(prompt):
+    print(ask(prompt))
