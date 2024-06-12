0125ab188efe3f1e1ea4f3f11d737491576fe0bb
Zebulun Arendsee <arendsee@users.noreply.github.com>
2024-06-11 21:23:33 -0400

    Read by chunks

--- src/puddle/cli/__init__.py
+++ src/puddle/cli/__init__.py
@@ -1,6 +1,8 @@
 import click
 from puddle.__about__ import __version__
-from puddle.llm import ask
+import puddle.llm as llm
+import puddle.collection as pc
+from termcolor import colored
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=False)
 @click.version_option(version=__version__, prog_name="puddle")
@@ -10,4 +12,29 @@ def puddle():
 @puddle.command(name = "ask")
 @click.argument("prompt", type=str)
 def ask_cmd(prompt):
-    print(ask(prompt))
+    print(llm.ask(prompt))
+
+def filterblank(txt):
+    lines = [x.rstrip() for x in txt.split("\n")]
+    return "\n".join([x for x in lines if len(x) > 0])
+
+
+@puddle.command(name = "lookup")
+@click.argument("query", type=str)
+@click.option("-n", "--nresults", type=int, default=3, help="Number of results to return")
+def lookup_cmd(query, nresults):
+    col = pc.opencol()
+    results = pc.query(
+        collection = col,
+        txt = query,
+        n = nresults
+    )
+
+    N = len(results["ids"])
+    for i in range(N):
+        M = len(results["ids"][i])
+        for j in range(M):
+            print("")
+            print(colored(results["ids"][i][j], "magenta"))
+            if results["documents"]:
+                print(filterblank(results["documents"][i][j]))
--- src/puddle/collection.py
+++ src/puddle/collection.py
@@ -1,11 +1,13 @@
 from puddle.config import load_config
 import glob
 import chromadb
+from chromadb.api.types import Metadata, OneOrMany
 import os.path
 import os
 import time
+from typing import List, Tuple
 
-supported_extensions = [".hs"]
+text_extensions = [".hs"]
 
 def opendb(path, collection):
     client = chromadb.PersistentClient(path=path)
@@ -18,21 +20,51 @@ def opencol() -> chromadb.Collection:
     collection = client.get_or_create_collection(name=config.collection)
     return collection
 
+def lines2chunks(lines : List[str], chunk_size : int = 30) -> List[str]:
+    chunks = []
+    for i in range(len(lines) // chunk_size):
+        chunks.append(lines[i*chunk_size:i*chunk_size+chunk_size])
+    return chunks
+
+
+def process_text(file : str) -> Tuple[List[str], List[str], OneOrMany[Metadata]]:
+    timestamp = time.ctime()
+    with open(file, "r") as fh:
+        ids = []
+        documents = []
+        metadatas = []
+        lineno = 0
+
+        for (i, chunk) in enumerate(lines2chunks(fh.readlines())):
+            ids.append(f"{file}:{lineno}-{lineno+len(chunk)}")
+            documents.append("".join(chunk))
+            metadatas.append({
+              "file" : file,
+              "timestamp" : str(timestamp),
+              "chunk_id" : str(i),
+              "total_chunks" : str(len(chunk))
+            })
+            lineno += len(chunk)
+
+        return (ids, documents, metadatas)
+
 def update() -> None:
     config = load_config()
     _, collection = opendb(str(config.chromadb), config.collection)
     for file in glob.iglob(os.path.join(config.datadir, "**"), recursive=True):
         extsplit = os.path.splitext(file)
-        if (len(extsplit) == 2 and extsplit[1] in supported_extensions):
-            with open(file, "r") as fh:
+        if (len(extsplit) == 2 and extsplit[1] in text_extensions):
+            (ids, documents, metadatas) = process_text(file)
+            # ignore empty files
+            if(len(ids) > 0):
                 collection.upsert(
-                    ids = [file],
-                    documents = [fh.read()],
-                    metadatas = [{"timestamp" : time.ctime()}],
+                    ids = ids,
+                    documents = documents,
+                    metadatas = metadatas,
                 )
 
-def query(collection : chromadb.Collection, txt : str) -> chromadb.QueryResult:
+def query(collection : chromadb.Collection, txt : str, n : int = 1) -> chromadb.QueryResult:
     return collection.query(
         query_texts=[txt],
-        n_results=3,
+        n_results=n,
     )
